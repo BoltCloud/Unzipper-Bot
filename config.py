cdce26f7370b629d2d76aac5fb38fd437ca86588
女神 <53878781+RomanGoddess@users.noreply.github.com>
2022-06-22 18:34:53 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 Itz-fork
+Copyright (c) 2022 Itz-fork
 
 import os
 
@@ -7,9 +7,10 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL"))
+    DB_CHANNEL = int(os.environ.get("DB_CHANNEL")) or LOGS_CHANNEL
     MONGODB_URL = os.environ.get("MONGODB_URL")
     BOT_OWNER = int(os.environ.get("BOT_OWNER"))
     DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/NexaBots"
     TG_MAX_SIZE = 2040108421
     # Default chunk size (0.005 MB) Increase if you need faster downloads
-    CHUNK_SIZE = 1024 * 6 
+    CHUNK_SIZE = 1024 * 6
